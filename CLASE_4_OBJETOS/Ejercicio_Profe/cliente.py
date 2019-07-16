595f6e4de0ded7508197c54a8a6b550af5d5d6ff
genny-tovar <52727174+genny-tovar@users.noreply.github.com>
2019-07-15 21:40:35 -0500

    Add files via upload

--- CLASE_4_OBJETOS/Ejercicio_Profe/cliente.py
+++ CLASE_4_OBJETOS/Ejercicio_Profe/cliente.py
@@ -1,9 +1,9 @@
 from temporizador import Temporizador
-from time import sleep
+from time import sleep #esto es para que vaya mas despacio cuando muestra el tiempo#
  
 t = Temporizador()
 
-t.iniciar([0,1,15])
+t.iniciar([0,1,15]) #aqui se pone el valor al que quiere llegar el temporizador#
 
 while True:
     tiempo = t.mostrar_tiempo()
